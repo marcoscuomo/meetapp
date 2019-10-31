95ef97b45345d4d89a91f60ba8fac00ba85c57bf
bytecore@gmail.com <marcos@mojumob.com.br>
2019-10-31 13:49:16 -0300

    Criando o loader de models

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,20 +1,19 @@
 import Sequelize, { Model } from 'sequelize';
 
-class User extends model {
-    static init(sequelize){
-        super.init(
-            {
-                name: Sequelize.STRING,
-                email: Sequelize.STRING,
-                password: Sequelize.STRING,
-                passwoard_hash: Sequelize.STRING,
-            }, 
-            {
-                sequelize,
-            }
-        );
-        }
-    }
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.STRING,
+        passwoard_hash: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
 }
 
 export default User;
