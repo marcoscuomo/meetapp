83bd2234bf4e4dacb05ddcf0ad7846e914f36205
bytecore@gmail.com <marcos@mojumob.com.br>
2019-10-31 11:24:15 -0300

    Criando a migration do usuario

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-    
-    return res.json({ msg: 'Chegou'});
+  return res.json({ msg: 'Chegou' });
 });
 
 export default routes;
