b3a0156755de3ef43b04b8e0af90537855d0574c
bytecore@gmail.com <marcos@mojumob.com.br>
2019-11-01 00:46:46 -0300

    Autenticacao com jwt

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  screat: '0dd0108a681fc69c23b07740dc115843',
+  experesIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import User from './app/models/User';
 import UserController from './app/controllers/UserController';
+import SessionControler from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionControler.store);
 
 routes.get('/', (req, res) => {
   return res.json({ msg: 'Chegou' });
