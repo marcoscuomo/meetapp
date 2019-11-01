1bd8944ffe5c9d66422dac76a238434fb77f943f
bytecore@gmail.com <marcos@mojumob.com.br>
2019-10-31 23:40:48 -0300

    Gerando o hash da senha

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,22 @@
 import { Router } from 'express';
+import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
+routes.post('/users', UserController.store);
+
 routes.get('/', (req, res) => {
   return res.json({ msg: 'Chegou' });
+});
+
+routes.get('/teste', async (req, res) => {
+  const user = await User.create({
+    name: 'Marcos Souza',
+    email: 'bytecore@gmail.com',
+    password_hash: '123',
+  });
+  return res.json(user);
 });
 
 export default routes;
